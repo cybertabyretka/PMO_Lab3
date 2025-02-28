1125fa346a63d218a07253d813780fcdd09cac80
Vlad Kalashnikov <kalashnikovv537@gmail.com>
2025-02-28 22:12:14 +0500

    refactoring train_model.bat

--- train_model.bat
+++ train_model.bat
@@ -2,4 +2,5 @@ echo Start train model
 cd C:/ProgramData/Jenkins/.jenkins/workspace/Download
 set PYTHON_PATH=C:\Users\333\AppData\Local\Programs\Python\Python310\python.exe
 venv/scripts/activate
+cd C:/
 %PYTHON_PATH% train_model.py > best_model.txt

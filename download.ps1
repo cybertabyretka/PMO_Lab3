fa3ddb33ce046c1f5cced2bf7e4e952f7e682d71
Vlad Kalashnikov <kalashnikovv537@gmail.com>
2025-02-28 22:07:34 +0500

    added train_model.bat

--- download.ps1
+++
@@ -1,7 +0,0 @@
-cd C:/ProgramData/Jenkins/.jenkins/workspace/Download
-python -m venv venv
-venv/scripts/activate
-python -m pip install --upgrade pip
-pip install setuptools
-pip install -r requirements.txt
-python download.py
---
+++ train_model.bat
@@ -0,0 +1,5 @@
+echo Start train model
+cd C:/ProgramData/Jenkins/.jenkins/workspace/Download
+set PYTHON_PATH=C:\Users\333\AppData\Local\Programs\Python\Python310\python.exe
+venv/scripts/activate
+%PYTHON_PATH% train_model.py > best_model.txt

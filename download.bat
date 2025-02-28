c5adac2de1814d1d283a2391bc61407b7e4fce69
Vlad Kalashnikov <kalashnikovv537@gmail.com>
2025-02-28 22:17:31 +0500

    refactoring train_model.bat and download.bat

--- download.bat
+++ download.bat
@@ -1,7 +1,7 @@
 cd C:/ProgramData/Jenkins/.jenkins/workspace/Download
 set PYTHON_PATH=C:\Users\333\AppData\Local\Programs\Python\Python310\python.exe
 %PYTHON_PATH% -m venv venv
-venv/scripts/activate
+venv\scripts\activate
 %PYTHON_PATH% -m pip install --upgrade pip
 %PYTHON_PATH% -m pip install setuptools
 %PYTHON_PATH% -m pip install -r requirements.txt

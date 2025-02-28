e81a02b9ed3b8e000aa447ec5cf95fc6053a99ca
Vlad Kalashnikov <kalashnikovv537@gmail.com>
2025-02-28 21:58:14 +0500

    refactor download.bat

--- download.bat
+++ download.bat
@@ -1,7 +1,8 @@
 cd C:/ProgramData/Jenkins/.jenkins/workspace/Download
-python -m venv venv
+set PYTHON_PATH=C:\Users\333\AppData\Local\Programs\Python\Python310\python.exe
+%PYTHON_PATH% -m venv venv
 venv/scripts/activate
-python -m pip install --upgrade pip
-pip install setuptools
-pip install -r requirements.txt
-python download.py
+%PYTHON_PATH% -m pip install --upgrade pip
+%PYTHON_PATH% -m pip install setuptools
+%PYTHON_PATH% -m pip install -r requirements.txt
+%PYTHON_PATH% download.py
